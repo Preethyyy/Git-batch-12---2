98dc5fbf1c52e6ba716143bad243640158045bc6
Preethyyy <preethisriram1802@gmail.com>
2022-11-24 15:53:30 -0800

    sam coded

--- src/org/file/sample.java
+++ src/org/file/sample.java
@@ -3,8 +3,12 @@ package org.file;
 public class sample {
 	public static void main(String[] args) {
 		
-	
+	sample s = new sample();
       int a =100;
       System.out.println(a);
+      
+      
+     int b = 200;
+     System.out.println(b);
 }
 	}
